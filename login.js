7f6acf2e7d2b088f9c07f6776b6a04f7a7d6e510
bakhbyergyen7 <95435746+bakhbyergyen7@users.noreply.github.com>
2022-11-19 15:54:40 -0600

    Add files via upload
    check() function working properly now

--- login.js
+++ login.js
@@ -4,26 +4,26 @@ function see()
 {
     var input = document.getElementById("psw");
     var see = document.getElementById("see");
-    
+
     if(is_visible)
     {
         input.type = 'psw';
-        is_visible = false; 
+        is_visible = false;
         see.style.color='gray';
     }
     else
     {
         input.type = 'text';
-        is_visible = true; 
+        is_visible = true;
         see.style.color='#262626';
     }
-    
+
 }
 
 function check()
 {
     var input = document.getElementById("psw").value;
-    
+
     input=input.trim();
     document.getElementById("psw").value=input;
     document.getElementById("count").innerText="Length : " + input.length;
@@ -33,34 +33,42 @@ function check()
     }
     else
     {
-       document.getElementById("check0").style.color="red"; 
+       document.getElementById("check0").style.color="red";
+       return false;
+       alert('Please make sure that your password length is at least 8 characters.')
     }
-    
+
     if(input.length<=32)
     {
         document.getElementById("check1").style.color="green";
     }
     else
     {
-       document.getElementById("check1").style.color="red"; 
+       document.getElementById("check1").style.color="red";
+       return false;
+       alert('Please make sure that your password length no more than 32 characters.');
     }
-    
+
     if(input.match(/[0-9]/i))
     {
         document.getElementById("check2").style.color="green";
     }
     else
     {
-       document.getElementById("check2").style.color="red"; 
+       document.getElementById("check2").style.color="red";
+       return false;
+       alert('Please make sure that your password contains a numerical value.')
     }
-    
+
     if(input.match(/[^A-Za-z0-9-' ']/i))
     {
         document.getElementById("check3").style.color="green";
     }
     else
     {
-       document.getElementById("check3").style.color="red"; 
+       document.getElementById("check3").style.color="red";
+       return false;
+       alert('Please make sure that your ppassword contains a special character.')
     }
-    
+
 }
