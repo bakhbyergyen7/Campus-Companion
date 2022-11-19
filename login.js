3fc62944eff291e3ec6100bb23dc68437b407f43
bakhbyergyen7 <95435746+bakhbyergyen7@users.noreply.github.com>
2022-11-18 23:01:02 -0600

    Add files via upload
    Added features in the password field that show the length of the password, if it satisfies certain criteria and more

---
+++ login.js
@@ -0,0 +1,66 @@
+var is_visible = false;
+
+function see()
+{
+    var input = document.getElementById("psw");
+    var see = document.getElementById("see");
+    
+    if(is_visible)
+    {
+        input.type = 'psw';
+        is_visible = false; 
+        see.style.color='gray';
+    }
+    else
+    {
+        input.type = 'text';
+        is_visible = true; 
+        see.style.color='#262626';
+    }
+    
+}
+
+function check()
+{
+    var input = document.getElementById("psw").value;
+    
+    input=input.trim();
+    document.getElementById("psw").value=input;
+    document.getElementById("count").innerText="Length : " + input.length;
+    if(input.length>=8)
+    {
+        document.getElementById("check0").style.color="green";
+    }
+    else
+    {
+       document.getElementById("check0").style.color="red"; 
+    }
+    
+    if(input.length<=32)
+    {
+        document.getElementById("check1").style.color="green";
+    }
+    else
+    {
+       document.getElementById("check1").style.color="red"; 
+    }
+    
+    if(input.match(/[0-9]/i))
+    {
+        document.getElementById("check2").style.color="green";
+    }
+    else
+    {
+       document.getElementById("check2").style.color="red"; 
+    }
+    
+    if(input.match(/[^A-Za-z0-9-' ']/i))
+    {
+        document.getElementById("check3").style.color="green";
+    }
+    else
+    {
+       document.getElementById("check3").style.color="red"; 
+    }
+    
+}
